97b5daa9ab8661395be0d72c8239377827433abb
Rawda Yasser <rawda.developer@gmail.com>
2022-02-06 12:38:37 +0200

    feat: add django admin interface
    add admin interface for Pet model for the admin to view and modify the Pet data + add string representation for the Vaccine model

--- main/models.py
+++ main/models.py
@@ -13,7 +13,8 @@ class Pet(models.Model):
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
-
+    def __str__(self):
+        return self.name
 
 
 
