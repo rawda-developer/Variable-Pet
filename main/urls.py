b6b41e16a59a75712c49aaff139415bcf452a399
Rawda Yasser <rawda.developer@gmail.com>
2022-02-19 12:07:42 +0200

    Chore: Add Pet Details template + modify home

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('adoptions/<int:pk>/', views.PetDetails.as_view(), name='pet.detail'),
+    path('adoptions/<int:pk>/', views.PetDetails.as_view(), name='pet.details'),
 ]
